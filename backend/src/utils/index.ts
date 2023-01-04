a20fd2902cbb2dab0923292f6293be42e12fd380
LayNath242 <nath.selendra@gmail.com>
2023-01-04 11:48:12 +0700

    add display name

--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -1,7 +1,20 @@
+import { DeriveAccountRegistration } from '@polkadot/api-derive/types';
 import { evmProvider } from './connect';
 import { logger } from './logger';
 import { LoggerOptions, ScanerConfig } from './types'
 import { getClient, dbQuery, dbParamQuery } from './db'
+
+const getDisplayName = (identity: DeriveAccountRegistration): string => {
+  if (
+    identity.displayParent &&
+    identity.displayParent !== '' &&
+    identity.display &&
+    identity.display !== ''
+  ) {
+    return `${identity.displayParent} / ${identity.display}`;
+  }
+  return identity.display || '';
+};
 
 export { 
   evmProvider,
@@ -11,4 +24,5 @@ export {
   dbQuery,
   dbParamQuery,
   ScanerConfig,
+  getDisplayName
 };
